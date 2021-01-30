8c4df663b998693bf14b5e2b1700117dfac1f5f0
Ítalo Penaforte <italopenaforte@gmail.com>
2021-01-29 21:19:29 -0300

    fix(test_app) finish test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import pytest
 from main import app as flask_app
+from main import _calculate_value_shipment
 import json
 
 
@@ -11,6 +12,25 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+def test_calculate_value_shipment():
+    valor_frete = 0.3
+    peso = 400
+    valor_total = _calculate_value_shipment(valor_frete, peso)
+    assert valor_total == 12.00
+
+
+def test_calculate_value_shipment_with_error():
+    valor_frete = 0
+    peso = 400
+    valor_total = _calculate_value_shipment(valor_frete, peso)
+    assert valor_total != 12.00
+
+
+def test_root_access(client):
+    res = client.get('/')
+    assert res.status_code == 200
 
 
 def test_calculate_shipping_retuning_two_companies(client):
@@ -34,6 +54,15 @@ def test_calculate_shipping_retuning_one_company(client):
 
 def test_calculate_shipping_returning_none_companies(client):
     product = {"dimensao": {"altura": 230, "largura": 162}, "peso": 5600}
+    expect = []
+    res = client.post('/calculate-shipping', json=product)
+    data = json.loads(res.get_data(as_text=True))
+    assert res.status_code == 200
+    assert expect == data
+
+
+def test_retun_none_passing_none_product(client):
+    product = {}
     expect = []
     res = client.post('/calculate-shipping', json=product)
     data = json.loads(res.get_data(as_text=True))
